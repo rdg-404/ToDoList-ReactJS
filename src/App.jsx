162c7c44b10189cf78a62dbd609e58fdf9a1e4a0
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-12-19 00:00:01 -0300

    add new task func

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './global.css'
 import { Header } from "./components/Header";
 import { Dashboard } from './components/Dashboard';
+import React from 'react';
 
 export function App() {
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,39 @@
+import React from 'react'
 import styles from './Dashboard.module.css'
 import Clipboard from '../assets/Clipboard.png'
 import { Plus } from 'phosphor-react'
+import { useState } from 'react'
 
 
 export function Dashboard (){
+
+    const [tasks, setTasks] = useState([])
+
+    const [input, setInput] = useState('')
+
+    function handleAddNewTask(){
+        setTasks([...tasks, input])
+    }
+
     return(
-        <>
+        <div>
             <div className={styles.wrapper}>
 
                 <div className={styles.input}>
-                    <input className={styles.inputMsg} type="text" placeholder='Adicione uma nova tarefa'/>
-                    <button className={styles.inputButton}>Criar <Plus/></button>
+                    <input 
+                        onChange={e => setInput(e.target.value)}
+                        value={input}
+                        className={styles.inputMsg} 
+                        type="text" 
+                        placeholder='Adicione uma nova tarefa'
+                    />
+
+                    <button 
+                        onClick={handleAddNewTask}
+                        type='submit' 
+                        className={styles.inputButton}>
+                            Criar <Plus/>
+                    </button>
                 </div>
 
 
@@ -20,6 +43,13 @@ export function Dashboard (){
                 </div>
 
                 
+                {tasks.map(task => {
+                    return (
+                        <span>{task}<br/></span>
+                    )
+                  
+                })}
+
                 <span className={styles.clipboard}>
                     <img src={Clipboard} alt="Clipboard" />
                 </span>
@@ -36,7 +66,7 @@ export function Dashboard (){
          
 
 
-        </>
+        </div>
      
     )
 }
